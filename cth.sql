ea4c803d731029ef2a97787b45b5748110bf1459
asyrofist <rakhasyrofist@gmail.com>
2019-10-26 22:47:18 +0700

    tambahan

---
+++ cth.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `motto` (
+  `motto_id` int(11) NOT NULL auto_increment,
+  `motto_title` varchar(50) NOT NULL,
+  `motto_deskripsi` varchar(10) NOT NULL,
+  PRIMARY KEY  (`motto_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
